000b2280e6951a7db4363b288adf66135f794f60
maxmaxjian <jianw812@gmail.com>
2016-11-08 19:48:08 -0800

    first edit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <utility>
+#include <unordered_set>
+
+using std::pair;
+
+class solution {
+public:
+  void solveSudoku(const std::vector<std::vector<int>> & board) {
+    
+  }
+};
+
+int main() {
+  
+}
